eb6d42bde03ee2a58e2971883dc120bc1a92b2bc
cste14 <emonnstu14@gmail.com>
2022-11-09 21:35:38 +0600

    login page with email password and google

---
+++ src/Component/Auth/Login.js
@@ -0,0 +1,102 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+
+const Login = () => {
+  const googleProvider = new GoogleAuthProvider();
+  const [email, setEmail] = useState("");
+  const [logUser, setLogUser] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const { auth, setLoading } = useContext(AuthContext);
+  //console.log(from);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        // console.log(user);
+        if (user.emailVerified) {
+          setLogUser(true);
+          setLoading(false);
+          toast("Login Successfuly");
+        }
+      })
+      .catch((error) => {
+        //console.error(error);
+        setLoading(false);
+        toast(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  const googleSignIn = () => {
+    setLoading(true);
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setLogUser(true);
+        setLoading(false);
+        toast("Login Successfuly");
+      })
+      .catch((error) => {
+        setLoading(false);
+        const errorMessage = error.message;
+        toast(errorMessage);
+        // The email of the user's account used.
+      });
+  };
+
+  return (
+    <div className="container my-5">
+      <div className="card p-4">
+        <h3 className="my-2">Login Form</h3>
+        <hr />
+        <form onSubmit={handleSubmit}>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control my-2"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control my-2"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <button className=" my-2 form-control btn btn-info">Login</button>
+          <ToastContainer />
+        </form>
+        <div className="otherLoginSystem">
+          <button
+            onClick={googleSignIn}
+            className=" my-2 form-control btn btn-primary"
+          >
+            Google
+          </button>
+        </div>
+      </div>
+      <div className="text-center my-2">
+        <small>
+          New user?{" "}
+          <strong>
+            <Link to="/user/register">Register</Link>
+          </strong>
+        </small>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
